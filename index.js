461454d53663e32fb407898d78ccce49c008916e
yoodongkun <dbehdrjs0707@naver.com>
2022-07-05 15:46:12 +0900

    function app fixed

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const app = express;
+const app = express();
 
 app.get('/',function(req,res){
-    res.send('Hello world!');
+    res.send('Hello world!')
 })
 
 app.listen(3000,()=>{
-    console.log('server is running on'+port);
+    console.log('server is running!')
 })
 
 
